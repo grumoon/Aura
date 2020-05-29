8cd04b676eab266a31ae97da7e23bcdc86476af6
xuzhouzhang <xuzhouzhang@tencent.com>
2020-05-29 14:52:15 +0800

    UIPasteboard

--- aura/aura/Module/Pasteboard/PasteboardViewController.m
+++ aura/aura/Module/Pasteboard/PasteboardViewController.m
@@ -8,7 +8,7 @@
 
 #import "PasteboardViewController.h"
 
-#define CUSTOM_PASTEBOARD_TYPE @"com.grumoon.test"
+#define CUSTOM_PASTEBOARD_TYPE @"com.grumoon.type"
 
 @interface PasteboardViewController ()
 
@@ -38,16 +38,6 @@
 -(IBAction)btnGetPasteboardInfoClick:(UIButton *)button {
     UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
     
-    // 唯品会读取的例子
-    NSString *pasteboardTypeVipShop = @"WECHAT_TIMELINE_AD_417200582";
-    NSData *vipShopData = [pasteboard dataForPasteboardType:pasteboardTypeVipShop];
-    if (vipShopData == nil) {
-        NSLog(@"no schema info for vipshop");
-    } else {
-        NSString *schemaInfoStr = [[NSString alloc] initWithData:vipShopData encoding:NSUTF8StringEncoding];
-        NSLog(@"schema info is %@", schemaInfoStr);
-    }
-    
     NSString *result = @"";
     
     result = [result stringByAppendingString:@"剪贴板基本信息\n"];
@@ -55,15 +45,13 @@
     
     result = [result stringByAppendingString:@"\n=========================\n"];
     result = [result stringByAppendingString:@"剪贴板最新一条数据的信息\n"];
-    result = [result stringByAppendingFormat:@"类型：%@\n", pasteboard.pasteboardTypes];
+    result = [result stringByAppendingFormat:@"类型：\n%@\n", pasteboard.pasteboardTypes];
     if([pasteboard.pasteboardTypes count] > 0) {
         NSString *pasteboardType = pasteboard.pasteboardTypes[0];
         if ([pasteboardType isEqualToString:CUSTOM_PASTEBOARD_TYPE]) {
-            result = [result stringByAppendingFormat:@"数据：%@\n", [[NSString alloc] initWithData:[pasteboard dataForPasteboardType:pasteboardType] encoding:NSUTF8StringEncoding]];
-        } else if ([pasteboardType hasPrefix:@"WECHAT_TIMELINE_AD_"]) {
-            result = [result stringByAppendingFormat:@"数据：%@\n", [[NSString alloc] initWithData:[pasteboard dataForPasteboardType:pasteboardType] encoding:NSUTF8StringEncoding]];
+            result = [result stringByAppendingFormat:@"数据：\n%@\n", [[NSString alloc] initWithData:[pasteboard dataForPasteboardType:pasteboardType] encoding:NSUTF8StringEncoding]];
         } else {
-            result = [result stringByAppendingFormat:@"数据：%@\n", [pasteboard valueForPasteboardType:pasteboardType]];
+            result = [result stringByAppendingFormat:@"数据：\n%@\n", [pasteboard valueForPasteboardType:pasteboardType]];
         }
     }
 
@@ -72,47 +60,44 @@
     result = [result stringByAppendingFormat:@"个数：%ld\n\n", pasteboard.numberOfItems];
     for (int i = 0; i < [pasteboard.items count]; i++) {
         result = [result stringByAppendingFormat:@"元素：%d\n", (i + 1)];
-        result = [result stringByAppendingFormat:@"类型：%@\n\n", pasteboard.items[i].allKeys];
+        result = [result stringByAppendingFormat:@"类型：\n%@\n\n", pasteboard.items[i].allKeys];
     }
-    
+
     self.lbPasteboardInfo.text = result;
+    
+    NSLog(@"%@", result);
 }
 
 
 -(IBAction)btnAddValue1Click:(UIButton *)button {
-    UIPasteboard *pasteBoard = [UIPasteboard generalPasteboard];
-    [pasteBoard setData:[@"schema://test.test.test" dataUsingEncoding:NSUTF8StringEncoding] forPasteboardType:@"com.grumoon.test"];
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    [pasteboard setData:[@"value for custom paste type" dataUsingEncoding:NSUTF8StringEncoding] forPasteboardType:CUSTOM_PASTEBOARD_TYPE];
     
 }
 
 -(IBAction)btnAddValue2Click:(UIButton *)button {
-    UIPasteboard *pasteBoard = [UIPasteboard generalPasteboard];
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    
+    if(pasteboard.numberOfItems >= 3) {
+        return;
+    }
+    
     NSMutableArray<NSDictionary<NSString *, id> *> *newItems = [NSMutableArray array];
     
     NSDictionary<NSString *, id> *newItemDic = [NSMutableDictionary dictionary];
-    [newItemDic setValue:@"123" forKey:@"key1-1"];
-    [newItemDic setValue:@"123" forKey:@"key1-2"];
-    [newItemDic setValue:@"123" forKey:@"key1-3"];
-    [newItemDic setValue:@"123" forKey:@"key1-4"];
+    [newItemDic setValue:@"1" forKey:@"key1-1"];
+    [newItemDic setValue:@"2" forKey:@"key1-2"];
     [newItems addObject:newItemDic];
     
     
     newItemDic = [NSMutableDictionary dictionary];
-    [newItemDic setValue:@"123" forKey:@"key2-1"];
-    [newItemDic setValue:@"123" forKey:@"key2-2"];
-    [newItemDic setValue:@"123" forKey:@"key2-3"];
-    [newItemDic setValue:@"123" forKey:@"key2-4"];
+    [newItemDic setValue:@"1" forKey:@"key2-1"];
+    [newItemDic setValue:@"2" forKey:@"key2-2"];
+    [newItemDic setValue:@"3" forKey:@"key2-3"];
+    [newItemDic setValue:@"4" forKey:@"key2-4"];
     [newItems addObject:newItemDic];
-    
-    newItemDic = [NSMutableDictionary dictionary];
-    [newItemDic setValue:@"123" forKey:@"key3-1"];
-    [newItemDic setValue:@"123" forKey:@"key3-2"];
-    [newItemDic setValue:@"123" forKey:@"key3-3"];
-    [newItemDic setValue:@"123" forKey:@"key3-4"];
-    [newItems addObject:newItemDic];
-    
-    
-    [pasteBoard addItems:newItems];
+
+    [pasteboard addItems:newItems];
     
     
 }
